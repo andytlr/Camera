fca74553ea2e11de4a188b15fc8d4727d3c1049a
Andy Taylor <andytlr@gmail.com>
2015-11-07 13:08:57 -0800

    Initial Commit

---
+++ Camera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Camera
+//
+//  Created by Andy Taylor on 11/7/15.
+//  Copyright © 2015 Andy Taylor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
