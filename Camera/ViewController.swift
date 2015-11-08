1a2efd21532909c0f0650cb51b3f6bcc93a721fa
Andy Taylor <andytlr@gmail.com>
2015-11-08 13:06:07 -0800

    Got a button and shit

--- Camera/ViewController.swift
+++ Camera/ViewController.swift
@@ -11,6 +11,9 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var cameraView: UIView!
+    @IBOutlet weak var recordButton: UIButton!
+    
     let captureSession = AVCaptureSession()
     var previewLayer: AVCaptureVideoPreviewLayer?
     var captureDevice: AVCaptureDevice?
@@ -22,14 +25,20 @@ class ViewController: UIViewController {
         captureSession.sessionPreset = AVCaptureSessionPresetHigh
         let devices = AVCaptureDevice.devices()
         
+        recordButton.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 0.2)
+        recordButton.layer.cornerRadius = 40;
+        recordButton.clipsToBounds = true;
+        recordButton.layer.borderWidth = 2;
+        recordButton.layer.borderColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 0.5).CGColor
+        
         print(devices)
         
         // Loop through all the capture devices on this phone
         for device in devices {
             // Make sure this particular device supports video
-            if (device.hasMediaType(AVMediaTypeVideo)) {
+            if device.hasMediaType(AVMediaTypeVideo) {
                 // Finally check the position and confirm we've got the back camera
-                if(device.position == AVCaptureDevicePosition.Back) {
+                if device.position == AVCaptureDevicePosition.Front {
                     captureDevice = device as? AVCaptureDevice
                 }
             }
@@ -47,7 +56,7 @@ class ViewController: UIViewController {
             captureSession.addInput(try AVCaptureDeviceInput(device: captureDevice!))
             
             previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-            self.view.layer.addSublayer(previewLayer!)
+            self.cameraView.layer.addSublayer(previewLayer!)
             previewLayer?.frame = self.view.layer.frame
             captureSession.startRunning()
             
@@ -63,10 +72,9 @@ class ViewController: UIViewController {
     }
     
     @IBAction func tapButton(sender: AnyObject) {
-        view.alpha = 0
-        view.backgroundColor = UIColor.whiteColor()
-        delay(0.1) {
-            self.view.alpha = 1
+        cameraView.alpha = 0
+        delay(0.085) {
+            self.cameraView.alpha = 1
         }
         if let videoConnection = stillImageOutput.connectionWithMediaType(AVMediaTypeVideo) {
             stillImageOutput.captureStillImageAsynchronouslyFromConnection(videoConnection) {
