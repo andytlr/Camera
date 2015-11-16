b4b417f9b03ac2f5fff0d8e6b652ce648ba132d8
Andy Taylor <andytlr@gmail.com>
2015-11-16 08:36:01 -0800

    Destroy all the views

--- Camera/PreviewViewController.swift
+++ Camera/PreviewViewController.swift
@@ -11,16 +11,28 @@ import AVKit
 import AVFoundation
 
 class PreviewViewController: UIViewController {
-
-    @IBOutlet weak var previewView: UIView!
     
+    let newPreviewView = UIView()
     let blackView = UIView()
+
+    let player = AVPlayer()
+    let playerLayer = AVPlayerLayer()
+    
+    var panPreview: UIPanGestureRecognizer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     override func viewWillAppear(animated: Bool) {
+        
+        self.view.backgroundColor = UIColor.clearColor()
+        newPreviewView.frame = self.view.bounds
+        view.insertSubview(newPreviewView, atIndex: 1)
+        newPreviewView.backgroundColor = UIColor.clearColor()
+        
+        panPreview = UIPanGestureRecognizer(target: self, action: "onPanPreview:")
+        newPreviewView.addGestureRecognizer(panPreview)
         
         let cameraRoll = returnContentsOfDocumentsDirectory()
         let latestItemInCameraRoll = String(cameraRoll.last!)
@@ -44,11 +56,10 @@ class PreviewViewController: UIViewController {
 //            let imageView = UIImageView(image: mirrorImage)
             
             imageView.frame = self.view.bounds
-            previewView.addSubview(imageView)
+            newPreviewView.addSubview(imageView)
             
         } else if latestFileFileExtension == ".mov" {
             
-            let playerLayer = AVPlayerLayer()
             playerLayer.frame = view.bounds
             
             let URL = NSURL(fileURLWithPath: filePath)
@@ -60,7 +71,7 @@ class PreviewViewController: UIViewController {
             playerLayer.player = player
             playerLayer.backgroundColor = UIColor.clearColor().CGColor
             playerLayer.videoGravity = AVLayerVideoGravityResize
-            previewView.layer.addSublayer(playerLayer)
+            newPreviewView.layer.addSublayer(playerLayer)
             player.play()
             
             NSNotificationCenter.defaultCenter().addObserver(self, selector: "playerDidReachEndNotificationHandler:", name: "AVPlayerItemDidPlayToEndTimeNotification", object: player.currentItem)
@@ -80,26 +91,27 @@ class PreviewViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    @IBAction func panPreviewView(sender: UIPanGestureRecognizer) {
+    
+    func onPanPreview(sender: UIScreenEdgePanGestureRecognizer) {
         
         let translation = sender.translationInView(view)
         let velocity = sender.velocityInView(view)
         
         if sender.state == .Began {
             view.backgroundColor = UIColor.clearColor()
-            previewView.backgroundColor = UIColor.clearColor()
+            newPreviewView.backgroundColor = UIColor.clearColor()
             
             blackView.frame = self.view.bounds
             view.insertSubview(blackView, atIndex: 0)
         }
         if sender.state == .Changed {
             
-            previewView.frame.origin.x = translation.x
-            previewView.frame.origin.y = translation.y
+            newPreviewView.frame.origin.x = translation.x
+            newPreviewView.frame.origin.y = translation.y
             
             let rotation = convertValue(translation.x, r1Min: 0, r1Max: view.frame.width, r2Min: 0, r2Max: 10)
 
-            previewView.transform = CGAffineTransformMakeDegreeRotation(rotation)
+            newPreviewView.transform = CGAffineTransformMakeDegreeRotation(rotation)
             
             let makeTransparentOnPan = convertValue(abs(translation.y), r1Min: (view.frame.height / 8), r1Max: (view.frame.height / 2), r2Min: 0.8, r2Max: 0)
             
@@ -122,22 +134,27 @@ class PreviewViewController: UIViewController {
             if velocity.y > 2000 || translation.y > (view.frame.height / 2) {
                 print("Delete Yo")
                 
-                let cameraRoll = returnContentsOfDocumentsDirectory()
-                let latestFileName = cameraRoll.last!.lastPathComponent!
-                removeItemFromDocumentsDirectory(latestFileName)
-                
                 UIView.animateWithDuration(dismissDuration, animations: { () -> Void in
                     
-                    self.previewView.frame.origin.y = self.view.frame.height * 1.3
-                    self.previewView.frame.origin.x += moveX
+                    self.newPreviewView.frame.origin.y = self.view.frame.height * 1.3
+                    self.newPreviewView.frame.origin.x += moveX
                     self.view.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 1)
                     
                     }, completion: { (Bool) -> Void in
                         
-                        // Need to delete the latest file here
-                        self.previewView.transform = CGAffineTransformMakeDegreeRotation(0)
+                        // Delete latest file.
+                        let cameraRoll = returnContentsOfDocumentsDirectory()
+                        let latestFileName = cameraRoll.last!.lastPathComponent!
+                        removeItemFromDocumentsDirectory(latestFileName)
                         
                         delay(0.1) {
+                            self.player.pause()
+                            self.playerLayer.removeFromSuperlayer()
+                            self.newPreviewView.transform = CGAffineTransformMakeDegreeRotation(0)
+                            self.newPreviewView.subviews.forEach({ $0.removeFromSuperview() })
+                            self.newPreviewView.removeFromSuperview()
+                            self.blackView.removeFromSuperview()
+                            self.view.backgroundColor = UIColor.clearColor()
                             self.view.removeFromSuperview()
                         }
                 })
@@ -147,15 +164,20 @@ class PreviewViewController: UIViewController {
                 
                 UIView.animateWithDuration(dismissDuration, animations: { () -> Void in
                     
-                    self.previewView.frame.origin.y = (self.view.frame.height * 1.3) * -1
-                    self.previewView.frame.origin.x += moveX
+                    self.newPreviewView.frame.origin.y = (self.view.frame.height * 1.3) * -1
+                    self.newPreviewView.frame.origin.x += moveX
                     self.view.backgroundColor = UIColor(red: 98/255, green: 217/255, blue: 98/255, alpha: 1)
                     
                     }, completion: { (Bool) -> Void in
                         
-                        self.previewView.transform = CGAffineTransformMakeDegreeRotation(0)
-                        
                         delay(0.1) {
+                            self.player.pause()
+                            self.playerLayer.removeFromSuperlayer()
+                            self.newPreviewView.transform = CGAffineTransformMakeDegreeRotation(0)
+                            self.newPreviewView.subviews.forEach({ $0.removeFromSuperview() })
+                            self.newPreviewView.removeFromSuperview()
+                            self.blackView.removeFromSuperview()
+                            self.view.backgroundColor = UIColor.clearColor()
                             self.view.removeFromSuperview()
                         }
                 })
@@ -163,9 +185,9 @@ class PreviewViewController: UIViewController {
             } else {
                 UIView.animateWithDuration(0.3, delay: 0, usingSpringWithDamping: 0.9, initialSpringVelocity: 10, options: [], animations: { () -> Void in
                     
-                    self.previewView.transform = CGAffineTransformMakeDegreeRotation(0)
-                    self.previewView.frame.origin.x = 0
-                    self.previewView.frame.origin.y = 0
+                    self.newPreviewView.transform = CGAffineTransformMakeDegreeRotation(0)
+                    self.newPreviewView.frame.origin.x = 0
+                    self.newPreviewView.frame.origin.y = 0
                     
                     }, completion: { (Bool) -> Void in
                         
