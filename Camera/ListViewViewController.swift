1ffb145bc205419ba05e166abfe0c62eb0978f98
Ben Ashman <ben.ashman@me.com>
2015-11-19 14:45:09 -0800

    show clips in table

--- Camera/ListViewViewController.swift
+++ Camera/ListViewViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class ListViewViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
@@ -16,44 +18,60 @@ class ListViewViewController: UIViewController, UITableViewDataSource, UITableVi
     var scenes: [String]!
     var scenetime: [String]!
     
+    var clips: [NSURL]!
+    var clipCount: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+//        scenes = ["Scene 1", "Scene 2", "Scene 3", "Scene 4", "Scene 5", "Scene 6", "Scene 7"]
+//        scenetime = ["00:01", "00:02", "00:03", "00:04", "00:05", "00:06", "00:07"]
         
-        //scene data
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(true)
         
-        scenes = ["Scene 1", "Scene 2", "Scene 3", "Scene 4", "Scene 5", "Scene 6", "Scene 7"]
-        scenetime = ["00:01", "00:02", "00:03", "00:04", "00:05", "00:06", "00:07"]
-        
-        //table delegates
-        
-        ClipReviewList.delegate = self
         ClipReviewList.dataSource = self
+        ClipReviewList.delegate = self
         
+        clips = returnContentsOfDocumentsDirectory()
+        clipCount = clips.count
+        print("Number of clips: \(clipCount)")
         
-        //getting contents of docs dir
-        
-        listContentsOfDocumentsDirectory()
-        
-       
+        ClipReviewList.reloadData()
     }
 
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return scenes.count
+        print(clipCount)
+        return clipCount
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        
         let cell = tableView.dequeueReusableCellWithIdentifier("SceneTableViewCell") as! SceneTableViewCell
         
-        let scenesname = scenes[indexPath.row]
-        let scenetimeduration = scenetime[indexPath.row]
-        
-        cell.SceneNumber.text = scenesname
-        cell.SceneDuration.text = scenetimeduration
+//        let scenesname = scenes[indexPath.row]
+//        let scenetimeduration = scenetime[indexPath.row]
+//        
+//        cell.SceneNumber.text = scenesname
+//        cell.SceneDuration.text = scenetimeduration
 
+        let clip = clips[indexPath.row]
+        let clipAsset = AVURLAsset(URL: clip)
+        let clipDuration = clipAsset.duration
+        let clipDurationInSeconds = Int(round(CMTimeGetSeconds(clipDuration)))
+        let clipDurationSuffix: String!
+        if clipDurationInSeconds == 1 {
+            clipDurationSuffix = "Second"
+        } else {
+            clipDurationSuffix = "Seconds"
+        }
+        
+        print(clipDuration)
+        
+        cell.SceneNumber.text = String(clip.absoluteString)
+        cell.SceneDuration.text = String("\(clipDurationInSeconds) \(clipDurationSuffix)")
         
         return cell
     }
