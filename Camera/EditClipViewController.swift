86a5dec5c23cdcf4c0a3bc087f8e5ac5e32960c3
Ben Ashman <ben.ashman@me.com>
2015-11-23 07:54:35 -0800

    pause video when exiting edit view

--- Camera/EditClipViewController.swift
+++ Camera/EditClipViewController.swift
@@ -229,6 +229,7 @@ class EditClipViewController: UIViewController, UITextFieldDelegate, UIGestureRe
             }
         }
         
+        player.pause()
         dismissViewControllerAnimated(false, completion: nil)
     }
     
