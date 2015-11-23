0a6cd2287f260db5b8542ab0cfc65c059baf34db
Andy Taylor <andytlr@gmail.com>
2015-11-22 20:47:55 -0800

    I dunno what's going on. This works the first time.

--- Camera/PreviewViewController.swift
+++ Camera/PreviewViewController.swift
@@ -192,9 +192,9 @@ class PreviewViewController: UIViewController {
                         try! realm.write {
                             realm.delete(self.clip)
                         }
-                        delay(0.25) {
-                            self.cameraViewController.updateButtonCount()
-                        }
+//                        delay(0.25) {
+//                            self.cameraViewController.updateButtonCount()
+//                        }
                 })
                 
             } else if velocity.y < -2000 || translation.y < (view.frame.height / 2) * -1 {
